2a14591eea86b01043934225f90ff44bf167adb8
Bill Sorem <billsorem@williams-mac-pro.local>
2017-05-24 15:06:10 -0500

    Initial Commit

---
+++ Bill Test/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Bill Test
+//
+//  Created by Bill Sorem on 5/24/17.
+//  Copyright © 2017 Bill Sorem. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
